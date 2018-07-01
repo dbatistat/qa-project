f970cba16256345b68ad371765abd206ff5f6219
David Batista <dbatista.t@gmail.com>
2018-07-01 18:16:14 -0400

    Added bdd test

--- carpinteart-bdd/features/login.feature
+++ carpinteart-bdd/features/login.feature
@@ -5,7 +5,23 @@ Feature: Iniciar sesion en sucursal
 
     Scenario: al iniciar con la tarea de sesion
     Given datos del usuario "vargas" con contraseña "vargas" 
-    When Envio estos datos con POST al servicio de carpinteart
-    Then El servidor responde con el estado 200 en ambas llamadas
-        And Con el token debe iniciar a la sucursal "SCZ"
+    When Envio estos datos con POST al servicio de autenticacion de carpinteart
+    Then El servidor responde con el estado 200 
+        And Con el token debe iniciar a la sucursal 1 en otro POST de autenticacion
         And La respuesta contiene el username "vargas"
+        And debe tener 1 permisos
+
+    Scenario Outline: al iniciar con la tarea de sesion
+    Given datos del usuario <username> con contraseña <password> 
+    When Envio estos datos con POST al servicio de autenticacion de carpinteart
+    Then El servidor responde con el estado <spected_status_code> 
+        And Con el token debe iniciar a la sucursal <branch_office_id> en otro POST de autenticacion
+        And La respuesta contiene el username <username>
+        And debe tener <permissions> permisos
+
+    Examples: 
+    |  username  |   password    | branch_office_id | spected_status_code |   permissions   | 
+    |  "vargas"  |  "vargas"     |       1          |       200           |        1        |
+    |  "vargas"  |  "123"        |       1          |       401           |        1        |
+    |  "admin"   |  "toro"       |       1          |       200           |        208      | 
+    |  "vargas"  |  "vargas"     |       10000      |       200           |        0        | 
--- carpinteart-bdd/features/support/login.steps.js
+++ carpinteart-bdd/features/support/login.steps.js
@@ -1,26 +1,89 @@
-import { Given, When, Then } from "cucumber";
+const { Given, When, Then } = require('cucumber')
+const { expect } = require('chai')
+const request = require('request-promise')
 
-Given('datos del usuario {string} con contraseña {string}', function (string, string2) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+let username;
+let password;
+let statusCode;
+let firstToken;
+
+let authResponse;
+
+Given('datos del usuario {string} con contraseña {string}', function (usernameParam, passwordParam) {
+  username = usernameParam;
+  password = passwordParam;
 });
 
-When('Envio estos datos con POST al servicio de carpinteart', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+When('Envio estos datos con POST al servicio de autenticacion de carpinteart', async function () {
+  let options = {
+    method: 'POST',
+    uri: 'http://localhost:9090/api/auth',
+    json: true,
+    headers: {
+      'username': username,
+      'password': password
+    },
+    resolveWithFullResponse: true
+  };
+
+  await request(options)
+    .then(function (response) {
+      authResponse = response;
+    })
+    .catch(function (error) {
+      authResponse = error;
+    });
 });
 
-Then('El servidor responde con el estado {int} en ambas llamadas', function (int) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then('El servidor responde con el estado {int}', function (code) {
+  expect(authResponse.statusCode).to.eql(code);
+  statusCode = code;
+
+
+  if (authResponse.statusCode == 200) {
+    firstToken = authResponse.body.token;
+    if (authResponse.body.branchOffices.length == 0) {
+      console.log(username + " no cuenta con sucursales disponibles");
+      expect(authResponse.body.branchOffices.length).to.eql(0);  
+    }
+  } 
 });
 
-Then('Con el token debe iniciar a la sucursal {string}', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then('Con el token debe iniciar a la sucursal {int} en otro POST de autenticacion', async function (branchOfficeId) {
+  if (statusCode != 200) {
+    return "skip";
+  }
+  let options = {
+    method: 'POST',
+    uri: 'http://localhost:9090/api/auth/branchOffice',
+    json: true,
+    headers: {
+      'branch_office': branchOfficeId,
+      'Authorization': "bearer " + firstToken
+    },
+    resolveWithFullResponse: true
+  };
+
+  await request(options)
+    .then(function (response) {
+      authResponse = response;
+    })
+    .catch(function (error) {
+      authResponse = error;
+    });
 });
 
-Then('La respuesta contiene el username {string}', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then('La respuesta contiene el username {string}', function (username) {
+  if (statusCode != 200) {
+    return "skip";
+  }
+  expect(authResponse.statusCode).to.eql(statusCode);
+  expect(authResponse.body.username).to.eql(username);
+});
+
+Then('debe tener {int} permisos', function (int) {
+  if (statusCode != 200) {
+    return "skip";
+  }
+  expect(authResponse.body.permissions.length).to.most(int);
 });
